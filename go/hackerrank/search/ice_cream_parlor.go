40447bc8a228ff2658721f12dc2228b87394dfdf
andre.scartezini <andre.scartezini@druid.com.br>
2021-10-25 23:39:59 -0300

    adding comments to ice cream parlor algo

--- go/hackerrank/search/ice_cream_parlor.go
+++ go/hackerrank/search/ice_cream_parlor.go
@@ -2,15 +2,20 @@ package hackerrank
 
 import "fmt"
 
+// this challenge is also known as two number sum
+
 func whatFlavors(cost []int32, money int32) {
 
 	visited := map[int32]int32{}
 
 	for i := 0; i < len(cost); i++ {
+		// checks if the result of the subtraction money - cost[i] is present in the map
+		// if present, prints the index stored for the result and the current index + 1
 		if _, present := visited[money-cost[i]]; present {
 			fmt.Printf("%d %d\n", visited[money-cost[i]], i+1)
 		}
 
+		// stores the visited number and its position in the array
 		visited[cost[i]] = int32(i + 1)
 	}
 }
